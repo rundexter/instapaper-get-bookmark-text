d75b301dd18b39e0478ba2126de7b07ead420ff2
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-18 12:33:09 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,42 @@
+var Instapaper = require('instapaper'),
+    _ = require('lodash'),
+    qs = require('querystring'),
+    util = require('./util.js');
+
+var apiUrl = 'https://www.instapaper.com/api/1.1';
+
+var pickInputs = {
+        'bookmark_id': 'bookmark_id'
+    };
+
 module.exports = {
+
+    /**
+     * Authorize module.
+     *
+     * @param dexter
+     * @returns {*}
+     */
+    authModule: function (dexter) {
+        var auth = {},
+            consumerKey = dexter.environment('instapaper_consumer_key'),
+            consumerSecret = dexter.environment('instapaper_consumer_secret'),
+
+            username = dexter.environment('instapaper_username'),
+            password = dexter.environment('instapaper_password');
+
+        if (consumerKey && consumerSecret && username && password) {
+            auth.consumerKey = consumerKey;
+            auth.consumerSecret = consumerSecret;
+            auth.user = username;
+            auth.pass = password;
+
+        } else {
+            this.fail('A [instapaper_consumer_key, instapaper_consumer_secret, instapaper_username, instapaper_password] environment need for this module.');
+        }
+
+        return _.isEmpty(auth)? false : auth;
+    },
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +44,27 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+
+        var auth = this.authModule(dexter),
+            client = Instapaper(auth.consumerKey, auth.consumerSecret, {apiUrl: apiUrl}),
+            inputs = util.pickStringInputs(step, pickInputs);
+
+        client.setUserCredentials(auth.user, auth.pass);
+
+        client.authenticate()
+            .bind(this)
+            .then(function(oauth) {
+                var opts = {body: qs.stringify(inputs)};
+                opts.oauth_token        = oauth.token;
+                opts.oauth_token_secret = oauth.secret;
+                opts.url                = apiUrl + '/bookmarks/get_text';
+
+                return client.POST(opts);
+            })
+            .spread(function(response, request) {
+                this.complete({ html: response });
+            }.bind(this)).catch(function(err) {
+                this.fail(err);
+            }.bind(this));
     }
 };
